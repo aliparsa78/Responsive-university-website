f412bc35eb822354853f2c1f863e19a62dacadc3
aliparsa <aliparsa9@gmail.com>
2025-01-05 10:21:12 +0430

    Course crud pard done' '

--- app/Http/Controllers/CoursController.php
+++ app/Http/Controllers/CoursController.php
@@ -21,7 +21,7 @@ class CoursController extends Controller
      */
     public function create()
     {
-        return view('Cours.create');
+        return view('Course.create');
     }
 
     /**
